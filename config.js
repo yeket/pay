66d15d8a3f6a6692a3905ed4e6d1a35c755d7091
Finale <yeket@users.noreply.github.com>
2023-05-18 18:00:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@
 const basic = { // 基础设置
     favicon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>', // 页面图标
     title: '(～￣▽￣)～', // 页面标题
-    avatar: 'https://gravatar.loli.net/avatar/', // 头像 URL
-    name: '这是一个昵称！', // 头像下的昵称
-    sign: '个性签名或者提示文字 ( •̀ ω •́ )✧', // 个性签名或提示文字，可使用 HTML 格式
-    user_page: 'https://github.com/ghost', // 点击头像或名字时跳转的链接，留空或删除则不跳转
-    footer: '这是一个页脚！', // 页脚文字，可使用 HTML 格式
+    avatar: 'http://cdnn.sgybbq.com/o_1h0n4ddbtgf8qi31far7466laa.jpg', // 头像 URL
+    name: '王亦可', // 头像下的昵称
+    sign: ' ( •̀ ω •́ )✧', // 个性签名或提示文字，可使用 HTML 格式
+    user_page: '', // 点击头像或名字时跳转的链接，留空或删除则不跳转
+    footer: '(づ￣ 3￣)づ', // 页脚文字，可使用 HTML 格式
     uri_redirect: false // 若收款码 URL 是网址，是否直接跳转而不显示二维码
 }
 
@@ -27,11 +27,11 @@ const urls = [ // 付款方式列表
     {
         name: '支付宝', // 名称
         ua: 'Alipay', // User-Agent 正则表达式
-        addr: 'https://qr.alipay.com/awa' // 收款码 URL
+        addr: 'https://qr.alipay.com/lpx09617mxupfshsdpegwc3' // 收款码 URL
     },
     {
         name: '微信',
         ua: 'MicroMessenger\/',
-        img: 'wechat.png' // 使用 img 而非 addr 参数以使用小程序码
+        img: 'http://cdnn.sgybbq.com/o_1h0n4hp6nnmp13k9pd21kv71geva.png' // 使用 img 而非 addr 参数以使用小程序码
     }
 ]
